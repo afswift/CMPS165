07743f729edfd1d43c5229d66ce2c807ed563993
AramisM <amarden@ucsc.edu>
2016-06-08 00:40:59 -0700

    add all legends w/changing values and units

--- WasteFunctions.js
+++ WasteFunctions.js
@@ -3,15 +3,15 @@ var quantizeWaste = d3.scale.quantize()
     .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
 
 var quantizeWPC = d3.scale.quantize()
-    .domain([0, 3])
+    .domain([0, 3.2])
     .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
     
 var quantizeOrganic = d3.scale.quantize()
-        .domain([0, 81])
+        .domain([0, 80])
         .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
 
 var quantizePaper = d3.scale.quantize()
-        .domain([0, 63])
+        .domain([0, 64])
         .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
 
 var quantizePlastic = d3.scale.quantize()
@@ -23,24 +23,25 @@ var quantizeGlass = d3.scale.quantize()
         .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
 
 var quantizeMetal = d3.scale.quantize()
-        .domain([0, 9])
+        .domain([0, 8])
         .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
 
 var quantizeOther = d3.scale.quantize()
-        .domain([0, 54])
+        .domain([0, 56])
         .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
 
 function Waste(){
+    svg.selectAll("g.legend").remove();
     d3.select("body").transition();
     
     // legend FOR WASTE
     var color_domain = [22500, 22500*2, 22500*3, 22500*4, 22500*5, 22500*6, 22500*7, 22500*8] // to corrolate with the 
     var ext_color_domain = [0, 22500, 22500*2, 22500*3, 22500*4, 22500*5, 22500*6, 22500*7, 22500*8]
-    var legend_labels = ["0-22499", "22500-44999", "45000-67499", "67500-89999", "90000-112499", "112500-134999", "135000-157499", "157500-179999", ">180000"];    
+    var legend_labels = ["<22499", "22500-44999", "45000-67499", "67500-89999", "90000-112499", "112500-134999", "135000-157499", "157500-179999", ">180000"];    
 
     var color = d3.scale.threshold()
         .domain(color_domain)
-        .range(["#F7FBFF","#DEEBF7","#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#08519C", "#08306B"]); //colors to show on legend
+        .range(["#F7FBFF","#DEEBF7","#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#08519C", "#08306B"]);
 
     var legend = svg.selectAll("g.legend")
         .data(ext_color_domain)
@@ -105,7 +106,43 @@ function Waste(){
 }
 
 function WPC(){
+    svg.selectAll("g.legend").remove();
     d3.select("body").transition();
+    
+    // legend FOR WASTE
+    var color_domain = [.4, .8, 1.2, 1.6, 2.0, 2.4, 2.8, 3.2]
+    var ext_color_domain = [0, .4, .8, 1.2, 1.6, 2.0, 2.4, 2.8, 3.2]
+    var legend_labels = ["<.39", ".40-.79", ".80-1.19", "1.20-1.59", "1.6-1.99", "2.00-2.39", "2.4-2.79", "2.8-3.19", ">3.2"];    
+
+    var color = d3.scale.threshold()
+        .domain(color_domain)
+        .range(["#F7FBFF","#DEEBF7","#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#08519C", "#08306B"]);
+
+    var legend = svg.selectAll("g.legend")
+        .data(ext_color_domain)
+        .enter().append("g")
+        .attr("class", "legend");
+
+    var ls_w = 20,
+        ls_h = 20;
+
+    legend.append("rect")
+          .attr("x", 20)
+          .attr("y", function(d, i){ return height - (i*ls_h) - 2*ls_h;})
+          .attr("width", ls_w)
+          .attr("height", ls_h)
+          .style("fill", function(d, i) { return color(d); })
+        //.style("opacity", 0.8);
+
+    legend.append("text")  
+         .attr("x", 50)
+         .attr("y", function(d, i){ return height - (i*ls_h) - ls_h - 4;})
+         .text(function(d, i){ return legend_labels[i]; });
+    
+    legend.append("text")
+         .attr("x", 19)
+         .attr("y", 300)
+         .text("Kg/Capita/Day");
         
     d3.json("final.json", function(error, world) {
     var countries = topojson.feature(world, world.objects.countries).features;
@@ -140,7 +177,74 @@ function WPC(){
 }
 
 function Organic(){
-    var svg = d3.select("body").transition();
+    svg.selectAll("g.legend").remove();
+    d3.select("body").transition();
+    
+    // legend
+    var color_domain = [10, 20, 30, 40, 50, 60, 70, 80]
+    var ext_color_domain = [0, 10, 20, 30, 40, 50, 60, 70, 80]
+    var legend_labels = ["<9.9", "10.0-19.0", "20.0-29.9", "30.0-39.9", "40.0-49.9", "50.0-59.9", "60.0-69.9", "70.0-79.9", ">80.0"];    
+
+    var color = d3.scale.threshold()
+        .domain(color_domain)
+        .range(["#F7FBFF","#DEEBF7","#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#08519C", "#08306B"]);
+
+    var legend = svg.selectAll("g.legend")
+        .data(ext_color_domain)
+        .enter().append("g")
+        .attr("class", "legend");
+
+    var ls_w = 20,
+        ls_h = 20;
+
+    legend.append("rect")
+          .attr("x", 20)
+          .attr("y", function(d, i){ return height - (i*ls_h) - 2*ls_h;})
+          .attr("width", ls_w)
+          .attr("height", ls_h)
+          .style("fill", function(d, i) { return color(d); })
+        //.style("opacity", 0.8);
+
+    legend.append("text")  
+         .attr("x", 50)
+         .attr("y", function(d, i){ return height - (i*ls_h) - ls_h - 4;})
+         .text(function(d, i){ return legend_labels[i]; });
+    
+    legend.append("text")
+         .attr("x", 19)
+         .attr("y", 300)
+         .text("% of total waste");
+        
+    d3.json("final.json", function(error, world) {
+    var countries = topojson.feature(world, world.objects.countries).features;
+        g.selectAll(".country")
+        .data(countries)
+        .enter().insert("path", ".graticule")
+        .attr("class", "country")
+        .attr("d", path)
+        .on("click", clicked)
+
+        .on("mouseover", function(d, i, test) {
+            div.transition()        
+                .duration(200)      
+                .style("opacity", .9);
+
+            div .html(				
+                    "<div style=\"text-align:center\">" +"<b>" +  d.id + "</b>" +  "</div>" +
+                    "<div style=\"float: left\">" + "Population: " + "</div>" +  "<div style=\"float:right\">" + d.properties.population + "</div>" + "<br/>" +
+                    "<div style=\"float: left\">" + "Waste: " + "</div>" +  "<div style=\"float:right\">" + d.properties.waste + " Tonnes/Day" +  "</div>" +"<br/>"  +
+                    "<div style=\"float: left\">" + "WPC: "+ "</div>" +  "<div style=\"float:right\">" + d.properties.wpc   +" Kg/Capita/Day"+  "</div>" +"<br/>"
+                 )  
+                .style("left", (d3.event.pageX) + "px")     
+                .style("top", (d3.event.pageY - 28) + "px");
+        })          
+        .on("mouseout", function(d) {
+            div .transition()		
+                .duration(500)		
+                .style("opacity", 0);	
+        })     
+        .attr("class", function(d) { return quantizeWPC(d.properties.wpc); });
+    })
     
     d3.json("final.json", function(error, world) {
     var countries = topojson.feature(world, world.objects.countries).features;
@@ -175,7 +279,42 @@ function Organic(){
 }
 
 function Paper(){
-    var svg = d3.select("body").transition();
+    svg.selectAll("g.legend").remove();
+    d3.select("body").transition();
+    
+    var color_domain = [8, 16, 24, 32, 40, 48, 56, 64]
+    var ext_color_domain = [ 0,  8, 16, 24, 32, 40, 48, 56, 64]
+    var legend_labels = ["<7.9", "8.0-15.9", "16.0-23.9", "24.0-31.9", "32.0-39.9", "40.0-47.9", "48.0-55.9", "56.0-63.9", ">64"];
+    
+     var color = d3.scale.threshold()
+        .domain(color_domain)
+        .range(["#F7FBFF","#DEEBF7","#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#08519C", "#08306B"]);
+
+    var legend = svg.selectAll("g.legend")
+        .data(ext_color_domain)
+        .enter().append("g")
+        .attr("class", "legend");
+
+    var ls_w = 20,
+        ls_h = 20;
+
+    legend.append("rect")
+          .attr("x", 20)
+          .attr("y", function(d, i){ return height - (i*ls_h) - 2*ls_h;})
+          .attr("width", ls_w)
+          .attr("height", ls_h)
+          .style("fill", function(d, i) { return color(d); })
+        //.style("opacity", 0.8);
+
+    legend.append("text")  
+         .attr("x", 50)
+         .attr("y", function(d, i){ return height - (i*ls_h) - ls_h - 4;})
+         .text(function(d, i){ return legend_labels[i]; });
+    
+    legend.append("text")
+         .attr("x", 19)
+         .attr("y", 300)
+         .text("% of total waste");
     
     d3.json("final.json", function(error, world) {
     var countries = topojson.feature(world, world.objects.countries).features;
@@ -210,7 +349,42 @@ function Paper(){
 }
 
 function Plastic(){
-    var svg = d3.select("body").transition();
+    svg.selectAll("g.legend").remove();
+    d3.select("body").transition();
+    
+    var color_domain = [  2.25,   4.5 ,   6.75,   9.  ,  11.25,  13.5 ,  15.75,  18.  ]
+    var ext_color_domain = [0, 2.25,   4.5 ,   6.75,   9.  ,  11.25,  13.5 ,  15.75,  18.  ]
+    var legend_labels = ["<2.24", "2.25-4.49", "4.5-6.74", "6.75-8.99", "9.00-11.24",  "11.25-13.49", "13.5-15.74", "15.75-17.99", ">18"];
+    
+     var color = d3.scale.threshold()
+        .domain(color_domain)
+        .range(["#F7FBFF","#DEEBF7","#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#08519C", "#08306B"]);
+
+    var legend = svg.selectAll("g.legend")
+        .data(ext_color_domain)
+        .enter().append("g")
+        .attr("class", "legend");
+
+    var ls_w = 20,
+        ls_h = 20;
+
+    legend.append("rect")
+          .attr("x", 20)
+          .attr("y", function(d, i){ return height - (i*ls_h) - 2*ls_h;})
+          .attr("width", ls_w)
+          .attr("height", ls_h)
+          .style("fill", function(d, i) { return color(d); })
+        //.style("opacity", 0.8);
+
+    legend.append("text")  
+         .attr("x", 50)
+         .attr("y", function(d, i){ return height - (i*ls_h) - ls_h - 4;})
+         .text(function(d, i){ return legend_labels[i]; });
+    
+    legend.append("text")
+         .attr("x", 19)
+         .attr("y", 300)
+         .text("% of total waste");
     
     d3.json("final.json", function(error, world) {
     var countries = topojson.feature(world, world.objects.countries).features;
@@ -245,7 +419,42 @@ function Plastic(){
 }
 
 function Glass(){
-    var svg = d3.select("body").transition();
+    svg.selectAll("g.legend").remove();
+    d3.select("body").transition();
+    
+    var color_domain = [1.5, 3.0, 4.5, 6.0, 7.5, 9., 10.5, 12]
+    var ext_color_domain = [0, 1.5, 3.0, 4.5, 6.0, 7.5, 9., 10.5, 12]
+    var legend_labels = ["<1.49", "1.50-2.99", "3.00-4.49", "4.50-5.99", "6.00-7.49", "7.50-8.99", "9.00-10.49", "10.50-1.99", ">12.00"];
+    
+     var color = d3.scale.threshold()
+        .domain(color_domain)
+        .range(["#F7FBFF","#DEEBF7","#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#08519C", "#08306B"]);
+
+    var legend = svg.selectAll("g.legend")
+        .data(ext_color_domain)
+        .enter().append("g")
+        .attr("class", "legend");
+
+    var ls_w = 20,
+        ls_h = 20;
+
+    legend.append("rect")
+          .attr("x", 20)
+          .attr("y", function(d, i){ return height - (i*ls_h) - 2*ls_h;})
+          .attr("width", ls_w)
+          .attr("height", ls_h)
+          .style("fill", function(d, i) { return color(d); })
+        //.style("opacity", 0.8);
+
+    legend.append("text")  
+         .attr("x", 50)
+         .attr("y", function(d, i){ return height - (i*ls_h) - ls_h - 4;})
+         .text(function(d, i){ return legend_labels[i]; });
+    
+    legend.append("text")
+         .attr("x", 19)
+         .attr("y", 300)
+         .text("% of total waste");
     
     d3.json("final.json", function(error, world) {
     var countries = topojson.feature(world, world.objects.countries).features;
@@ -280,7 +489,43 @@ function Glass(){
 }
 
 function Metal(){
-    var svg = d3.select("body").transition();
+    svg.selectAll("g.legend").remove();
+    d3.select("body").transition();
+    
+    // legend
+    var color_domain = [1, 2, 3, 4, 5, 6, 7, 8]
+    var ext_color_domain = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    var legend_labels = ["<0.9", "1.0-1.9", "2.0-2.9", "3.0-3.9", "4.0-4.9", "5.0-5.9", "6.0-6.9", "7.0-7.9", ">8.0"];     
+
+    var color = d3.scale.threshold()
+        .domain(color_domain)
+        .range(["#F7FBFF","#DEEBF7","#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#08519C", "#08306B"]);
+
+    var legend = svg.selectAll("g.legend")
+        .data(ext_color_domain)
+        .enter().append("g")
+        .attr("class", "legend");
+
+    var ls_w = 20,
+        ls_h = 20;
+
+    legend.append("rect")
+          .attr("x", 20)
+          .attr("y", function(d, i){ return height - (i*ls_h) - 2*ls_h;})
+          .attr("width", ls_w)
+          .attr("height", ls_h)
+          .style("fill", function(d, i) { return color(d); })
+        //.style("opacity", 0.8);
+
+    legend.append("text")  
+         .attr("x", 50)
+         .attr("y", function(d, i){ return height - (i*ls_h) - ls_h - 4;})
+         .text(function(d, i){ return legend_labels[i]; });
+    
+    legend.append("text")
+         .attr("x", 19)
+         .attr("y", 300)
+         .text("% of total waste");
     
     d3.json("final.json", function(error, world) {
     var countries = topojson.feature(world, world.objects.countries).features;
@@ -315,7 +560,43 @@ function Metal(){
 }
 
 function Other(){
-    var svg = d3.select("body").transition();
+    svg.selectAll("g.legend").remove();
+    d3.select("body").transition();
+    
+    // legend
+    var color_domain = [7, 14, 21, 28, 35, 42, 49, 56]
+    var ext_color_domain = [0, 7, 14, 21, 28, 35, 42, 49, 56]
+    var legend_labels = ["<6.9", "7.0-13.9", "14.0-20.9", "21.0-27.9", "28.0-34.9", "35.0-41.9", "42.0-48.9", "49.0-55.9", ">56.0"];    
+
+    var color = d3.scale.threshold()
+        .domain(color_domain)
+        .range(["#F7FBFF","#DEEBF7","#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#08519C", "#08306B"]);
+
+    var legend = svg.selectAll("g.legend")
+        .data(ext_color_domain)
+        .enter().append("g")
+        .attr("class", "legend");
+
+    var ls_w = 20,
+        ls_h = 20;
+
+    legend.append("rect")
+          .attr("x", 20)
+          .attr("y", function(d, i){ return height - (i*ls_h) - 2*ls_h;})
+          .attr("width", ls_w)
+          .attr("height", ls_h)
+          .style("fill", function(d, i) { return color(d); })
+        //.style("opacity", 0.8);
+
+    legend.append("text")  
+         .attr("x", 50)
+         .attr("y", function(d, i){ return height - (i*ls_h) - ls_h - 4;})
+         .text(function(d, i){ return legend_labels[i]; });
+    
+    legend.append("text")
+         .attr("x", 19)
+         .attr("y", 300)
+         .text("% of total waste");
     
     d3.json("final.json", function(error, world) {
     var countries = topojson.feature(world, world.objects.countries).features;
